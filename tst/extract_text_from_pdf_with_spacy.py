586b762bfdd453618eb42f038b8354dbd62fb9ba
stakaguessu <stakaguessu@enseirb-matemca.fr>
2025-01-09 15:55:58 +0100

    eh

--- tst/extract_text_from_pdf_with_spacy.py
+++ tst/extract_text_from_pdf_with_spacy.py
@@ -3,6 +3,8 @@ import spacy
 from spacypdfreader import pdf_reader
 
 nlp = spacy.load("en_core_web_sm")
-doc = pdf_reader("tests/data/test_pdf_01.pdf", nlp)
+doc = pdf_reader("src/finetuning/data/articles/01-Petit-181-216.pdf", nlp)
 
-print(doc)
+# print(doc)
+# print(doc._.first_page)
+print(doc._.page(1))
